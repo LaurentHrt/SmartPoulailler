b8de956193cccfe98c13e9a2ebae0c817164055e
Laurent <laurent.hirth.68@gmail.com>
2019-04-27 16:19:44 +0200

    Alternance Led Verte Led Rouge sur appui d'un bouton

--- smartPoulailler.ino
+++ smartPoulailler.ino
@@ -1,9 +1,46 @@
+char pinLedRouge;
+char pinLedVerte;
+char pinBouton;
+bool etatBouton, dernierEtatBouton;
+bool porteOuverte;
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  pinLedRouge=2;
+  pinLedVerte=3;
+  pinBouton=12;
+
+  etatBouton=LOW;
+  dernierEtatBouton=HIGH;
+  porteOuverte=LOW;
+
+  pinMode(pinLedRouge,OUTPUT);
+  pinMode(pinLedVerte,OUTPUT);
+  pinMode(pinBouton,INPUT_PULLUP);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  etatBouton=digitalRead(pinBouton);
+  Serial.println(etatBouton);
+
+  if(etatBouton!=dernierEtatBouton && etatBouton==LOW) {
+    if(porteOuverte) {
+      digitalWrite(pinLedRouge,HIGH);
+      digitalWrite(pinLedVerte,LOW);
+    }
+    else {
+      digitalWrite(pinLedRouge,LOW);
+      digitalWrite(pinLedVerte,HIGH);
+    }
+    porteOuverte=!porteOuverte;
+  }
+
+  dernierEtatBouton=etatBouton;
+
+  delay(200);
 
 }
