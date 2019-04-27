3cfb72deb92435504001122857824c4ac36f7571
Laurent <laurent.hirth.68@gmail.com>
2019-04-27 16:44:33 +0200

    Led rouge et verte en fonction de la lumière

--- smartPoulailler.ino
+++ smartPoulailler.ino
@@ -1,8 +1,12 @@
 char pinLedRouge;
 char pinLedVerte;
 char pinBouton;
+char pinPhotoCell;
+
 bool etatBouton, dernierEtatBouton;
 bool porteOuverte;
+
+int etatPhotoCell;
 
 void setup() {
 
@@ -11,6 +15,7 @@ void setup() {
   pinLedRouge=2;
   pinLedVerte=3;
   pinBouton=12;
+  pinPhotoCell=0;
 
   etatBouton=LOW;
   dernierEtatBouton=HIGH;
@@ -25,7 +30,9 @@ void setup() {
 void loop() {
 
   etatBouton=digitalRead(pinBouton);
-  Serial.println(etatBouton);
+  etatPhotoCell=analogRead(pinPhotoCell);
+  Serial.println(etatPhotoCell);
+  //Serial.println(etatBouton);
 
   if(etatBouton!=dernierEtatBouton && etatBouton==LOW) {
     if(porteOuverte) {
@@ -40,6 +47,15 @@ void loop() {
   }
 
   dernierEtatBouton=etatBouton;
+
+  if(etatPhotoCell>500) {
+    digitalWrite(pinLedRouge,HIGH);
+    digitalWrite(pinLedVerte,LOW);
+  }
+  else {
+    digitalWrite(pinLedRouge,LOW);
+    digitalWrite(pinLedVerte,HIGH);
+  }
 
   delay(200);
 
