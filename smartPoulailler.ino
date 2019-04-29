2e23f569b1d7b92a473ef29304e254edec9cf4cd
Laurent Hirth <laurent.hirth@protonmail.com>
2019-04-29 16:11:19 +0200

    Fonction calculMode et Commentaires

--- smartPoulailler.ino
+++ smartPoulailler.ino
@@ -1,58 +1,53 @@
+// Declaration des includes
 #include <LiquidCrystal.h>
 
+// Declaration des constantes
 const byte pinLedRouge=2, pinLedVerte=3;
 const byte pinBuzzer=22;
 const byte pinBoutonPorte=12, pinBoutonMode=11, pinPhotoCell=0;
 
-byte mode; //1: Mode bouton, 2: Mode Luminosité, 3: Mode horaire
-
+// Declaration des variables globales
+byte mode; // 1: Mode bouton, 2: Mode Luminosité, 3: Mode horaire
 bool etatBouton, dernierEtatBouton, etatBoutonMode, dernierEtatBoutonMode;
 bool porteOuverte;
-
 int etatPhotoCell;
 
-// initialize the library with the numbers of the interface pins
+// Declaration du LCD (numero de pin)
 LiquidCrystal lcd(38, 39, 40, 41, 42, 43);
 
 void setup() {
 
+  // Initialisation des vaiables
   mode=1;
-
   etatBouton=LOW;
   dernierEtatBouton=HIGH;
   etatBoutonMode=HIGH;
   dernierEtatBoutonMode=HIGH;
   porteOuverte=LOW;
-
+  
+  // Initialisation du mode des PIN
   pinMode(pinLedRouge,OUTPUT);
   pinMode(pinLedVerte,OUTPUT);
-
   pinMode(pinBuzzer,OUTPUT);
-  digitalWrite(pinBuzzer,HIGH);
-
   pinMode(pinBoutonPorte,INPUT_PULLUP);
   pinMode(pinBoutonMode,INPUT_PULLUP);
+  
+  // Initialisation de l'etat des PIN
+  digitalWrite(pinBuzzer,HIGH);
 
-  // set up the LCD's number of columns and rows:
+  // Initialisation du LCD
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("Smart Poulailler");
 
 }
 
+
 void loop() {
 
-  // ************* Gestion du mode *******************
-  etatBoutonMode=digitalRead(pinBoutonMode);
-  if(etatBoutonMode!=dernierEtatBoutonMode && etatBoutonMode==LOW) {
-
-    buzz(100);
-
-    mode++;
-    if (mode > 3)
-      mode=1;
-  }
-
+  // Caclul du mode
+  mode=calculMode();
+ 
+  // Selection du mode
   switch (mode)
   {
     case 1: // Mode 1 : Bouton
@@ -73,20 +68,46 @@ void loop() {
 
 }
 
+// Fonction : Lecture du bouton mode
+// Return : mode
+byte calculMode() {
+  
+  // Lecture du bouton
+  etatBoutonMode=digitalRead(pinBoutonMode);
+  
+  // Gestion du bouton
+  if(etatBoutonMode!=dernierEtatBoutonMode && etatBoutonMode==LOW) {
+    buzz(100);
+    mode++;
+    if (mode > 3)
+      mode=1;
+  }
+  
+  // Mise en memoire du dernier etat du bouton
+  dernierEtatBoutonMode=etatBoutonMode
+  
+  return mode;
+  
+}
+
 // Fonction : Mode 1 : Bouton
 void modeBouton() {
 
+  // Lecture du bouton
   etatBouton=digitalRead(pinBoutonPorte);
 
+  // Mise a jour du LCD
   lcd.setCursor(0, 1);
   lcd.print("Mode manu : ");
   lcd.print(etatBouton);
   lcd.print("      ");
 
+  // Gestion du bouton
   if(etatBouton!=dernierEtatBouton && etatBouton==LOW) {
     ouverturePorte(!porteOuverte);
   }
 
+  // Mise en memoire du dernier etat du bouton
   dernierEtatBouton=etatBouton;
 
 }
@@ -94,24 +115,28 @@ void modeBouton() {
 // Fonction : Mode 2 : Luminosite
 void modeLuminosite() {
 
+  // Lecture de la photocell
   etatPhotoCell=analogRead(pinPhotoCell);
 
+  // Mise a jour du LCD
   lcd.setCursor(0, 1);
   lcd.print("Mode auto : ");
   lcd.print(etatPhotoCell);
 
+  // Gestion de la photocell
   if(etatPhotoCell>500)
     ouverturePorte(true);
   else
     ouverturePorte(false);
 
-    delay(200);
+  delay(200);
 
 }
 
 // Fonction : Mode 3 : Horaire
 void modeHorraire() {
 
+  // Mise a jour du LCD
   lcd.setCursor(0, 1);
   lcd.print("Mode heur :         ");
 
