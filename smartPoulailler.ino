6cd053959930a51052f9042d11a3372b33facf5d
Laurent <laurent.hirth.68@gmail.com>
2019-05-03 17:32:01 +0200

    Essais non fontionnels

--- smartPoulailler.ino
+++ smartPoulailler.ino
@@ -3,6 +3,8 @@
 #include <Wire.h>           // RTC
 #include <RTClib.h>         // RTC
 #include <SimpleDHT.h>      // Temp + Humidity
+#include <math.h>
+
 
 // Declaration des constantes
 const byte pinLedRouge=2, pinLedVerte=3;
@@ -37,6 +39,10 @@ LiquidCrystal lcd(38, 39, 40, 41, 42, 43);
 
 // Declaration de l'horloge
 RTC_DS3231 rtc;
+
+
+  double lever, meridien, coucher;
+  DateTime test;
 
 void setup() {
 
@@ -76,9 +82,25 @@ void setup() {
 
   buzz(100);
 
+
+  test=rtc.now();
+  calculerEphemeride(test.day(), test.month(), test.year(), 47.730420, -7.148910, &lever, &meridien, &coucher);
+
+
+
 }
 
 void loop() {
+
+
+    Serial.print("Lever : ");
+    afficherHeure(lever);
+    Serial.println("");
+    Serial.print("Coucher : ");
+    afficherHeure(coucher);
+    Serial.println("");
+
+
 
   currentMillis = millis();
 
@@ -260,4 +282,130 @@ void buzz(char duree) {
   digitalWrite(pinBuzzer,LOW);
   delay(duree);
   digitalWrite(pinBuzzer,HIGH);
+}
+
+
+
+
+
+
+void calculerCentreEtVariation(double longitude_ouest, double sinlat, double coslat, double d, double *centre, double *variation)
+{
+  //constantes précalculées par le compilateur
+  const double M_2PI = 2.0 * M_PI;
+  const double degres = 180.0 / M_PI;
+  const double radians = M_PI / 180.0;
+  const double radians2 = M_PI / 90.0;
+  const double m0 = 357.5291;
+  const double m1 = 0.98560028;
+  const double l0 = 280.4665;
+  const double l1 = 0.98564736;
+  const double c0 = 0.01671;
+  const double c1 = degres * (2.0*c0 - c0*c0*c0/4.0);
+  const double c2 = degres * c0*c0 * 5.0 / 4.0;
+  const double c3 = degres * c0*c0*c0 * 13.0 / 12.0;
+  const double r1 = 0.207447644182976; // = tan(23.43929 / 180.0 * M_PI / 2.0)
+  const double r2 = r1*r1;
+  const double d0 = 0.397777138139599; // = sin(23.43929 / 180.0 * M_PI)
+  const double o0 = -0.0106463073113138; // = sin(-36.6 / 60.0 * M_PI / 180.0)
+
+  double M,C,L,R,dec,omega,x;
+
+  //deux ou trois petites formules de calcul
+  M = radians * fmod(m0 + m1 * d, 360.0);
+  C = c1*sin(M) + c2*sin(2.0*M) + c3*sin(3.0*M);
+  L = fmod(l0 + l1 * d + C, 360.0);
+  x = radians2 * L;
+  R = -degres * atan((r2*sin(x))/(1+r2*cos(x)));
+  *centre = (C + R + longitude_ouest)/360.0;
+
+  dec = asin(d0*sin(radians*L));
+  omega = (o0 - sin(dec)*sinlat)/(cos(dec)*coslat);
+  if ((omega > -1.0) && (omega < 1.0))
+    *variation = acos(omega) / M_2PI;
+  else
+    *variation = 0.0;
+}
+
+void calculerEphemeride(int jour, int mois, int annee, double longitude_ouest, double latitude_nord, double *lever, double *meridien, double *coucher)
+{
+  int nbjours;
+  const double radians = M_PI / 180.0;
+  double d, x, sinlat, coslat;
+
+  //calcul nb jours écoulés depuis le 01/01/2000
+  if (annee > 2000) annee -= 2000;
+  nbjours = (annee*365) + ((annee+3)>>2) + jour - 1;
+  switch (mois)
+  {
+    case  2 : nbjours +=  31; break;
+    case  3 : nbjours +=  59; break;
+    case  4 : nbjours +=  90; break;
+    case  5 : nbjours += 120; break;
+    case  6 : nbjours += 151; break;
+    case  7 : nbjours += 181; break;
+    case  8 : nbjours += 212; break;
+    case  9 : nbjours += 243; break;
+    case 10 : nbjours += 273; break;
+    case 11 : nbjours += 304; break;
+    case 12 : nbjours += 334; break;
+  }
+  if ((mois > 2) && (annee&3 == 0)) nbjours++;
+  d = nbjours;
+
+  //calcul initial meridien & lever & coucher
+  x = radians * latitude_nord;
+  sinlat = sin(x);
+  coslat = cos(x);
+  calculerCentreEtVariation(longitude_ouest, sinlat, coslat, d + longitude_ouest/360.0, meridien, &x);
+  *lever = *meridien - x;
+  *coucher = *meridien + x;
+
+  //seconde itération pour une meilleure précision de calcul du lever
+  calculerCentreEtVariation(longitude_ouest, sinlat, coslat, d + *lever, lever, &x);
+  *lever = *lever - x;
+
+  //seconde itération pour une meilleure précision de calcul du coucher
+  calculerCentreEtVariation(longitude_ouest, sinlat, coslat, d + *coucher, coucher, &x);
+  *coucher = *coucher + x;
+}
+
+void afficherHeure(double d)
+{
+  int h,m,s;
+  char texte[20];
+
+  d = d + 0.5;
+
+  if (d < 0.0)
+  {
+    d = d + 1.0;
+  }
+  else
+  {
+    if (d > 1.0)
+    {
+      d = d - 1.0;
+    }
+    else
+    {
+
+    }
+  }
+
+  h = d * 24.0;
+  d = d - (double) h / 24.0;
+  m = d * 1440.0;
+  d = d - (double) m / 1440.0;
+  s = d * 86400.0 + 0.5;
+
+  sprintf(texte,"%02d:%02d:%02d",h,m,s);
+  Serial.print(texte);
+
+  // Serial.println(texte,"%02d:%02d:%02d",h,m,s);
+  // Serial.print(h);
+  // Serial.print(":");
+  // Serial.print(m);
+  // Serial.print(":");
+  // Serial.print(s);
 }
